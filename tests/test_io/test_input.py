ae3c9f4a3a45b0938d7a0ae7cc05c04b3b550d74
Julia Livakina <yulialivakina@gmail.com>
2026-03-10 19:19:22 +0200

    Add unit tests for read_data_from_file function

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -28,7 +28,25 @@ class TestInputFunctions(unittest.TestCase):
         result = read_text_from_file("multi.txt")
         self.assertEqual(result, "Hello\nWorld")
 
+ # тести для read_data_from_file
 
+    def test_read_csv_correct(self):
+        df = pd.DataFrame({"name": ["Anna"], "age": [20]})
+        df.to_csv("test.csv", index=False)
+        result = read_data_from_file("test.csv")
+        self.assertEqual(result.iloc[0]["name"], "Anna")
+
+    def test_read_csv_rows(self):
+        df = pd.DataFrame({"name": ["Anna", "Ivan"], "age": [20, 22]})
+        df.to_csv("test2.csv", index=False)
+        result = read_data_from_file("test2.csv")
+        self.assertEqual(len(result), 2)
+
+    def test_read_csv_columns(self):
+        df = pd.DataFrame({"name": ["Anna"], "age": [20]})
+        df.to_csv("test3.csv", index=False)
+        result = read_data_from_file("test3.csv")
+        self.assertIn("name", result.columns)
 
 if __name__ == "__main__":
     unittest.main()
