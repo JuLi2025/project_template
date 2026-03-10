c607aa0e41fb854e9b57417c02e69c9f4b219505
Julia Livakina <yulialivakina@gmail.com>
2026-03-10 16:41:56 +0200

    Add input and output function

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+# Функція для вводу тексту з консолі
+def input_from_console():
+    pass
+
+# Функція для зчитування тексту з файлу за допомогою вбудованих можливостей python
+def read_text_ from_file(file_path):
+    pass
+
+# Функція для зчитування даних з файлу за допомогою бібліотеки pandas
+def read_data_from_file(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+# Функція для виводу тексту у консоль
+def output_to_console(text):
+    pass
+
+# Функція для запису тексту у файл за допомогою вбудованих можливостей python
+def write_to_file(file_path, text):
+    pass
